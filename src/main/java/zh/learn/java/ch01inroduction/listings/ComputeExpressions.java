1446beb7881563fa64a03547a313ee3065879290
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2019-12-08 15:57:06 +0000

    Chapter 01, listings

---
+++ src/main/java/zh/learn/java/ch01inroduction/listings/ComputeExpressions.java
@@ -0,0 +1,8 @@
+package zh.learn.java.ch01inroduction.listings;
+
+public class ComputeExpressions {
+    public static void main(String[] args) {
+        System.out.print("(10.5 + 2 * 3) / (45 - 3.5) = ");
+        System.out.println((10.5 + 2 * 3) / (45 - 43.5));
+    }
+}
---
+++ src/main/java/zh/learn/java/ch01inroduction/listings/WelcomeWithThreeMessages.java
@@ -0,0 +1,9 @@
+package zh.learn.java.ch01inroduction.listings;
+
+public class WelcomeWithThreeMessages {
+    public static void main(String[] args) {
+        System.out.println("Programming is fun!");
+        System.out.println("Fundamentals First");
+        System.out.println("Problem Driven");
+    }
+}
