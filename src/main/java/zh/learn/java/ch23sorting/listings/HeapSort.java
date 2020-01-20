bedff4db21177a4234af37759722b1a6d86727a4
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-01-20 07:33:20 +0000

    HeapSort

---
+++ src/main/java/zh/learn/java/ch23sorting/listings/HeapSort.java
@@ -0,0 +1,23 @@
+package zh.learn.java.ch23sorting.listings;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class HeapSort {
+    public static <E extends Comparable<E>> void sort(E[] list) {
+        Heap<E> heap = new Heap<>();
+
+        for (int i = 0; i < list.length; ++i)
+            heap.add(list[i]);
+
+        for (int i = list.length - 1; i >= 0; --i)
+            list[i] = heap.remove();
+    }
+
+    public static void main(String[] args) {
+        Integer[] list = { 2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+        sort(list);
+        for (int i = 0; i < list.length; ++i)
+            System.out.print(list[i] + " ");
+    }
+}
