96d8451b31c62ebecb70df8dc4c1a3c7052ad7f6
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-11-29 12:59:32 +0000

    Section 2: CalculatingEnergy

---
+++ src/main/java/zh/learn/java/ch02elementary/exercises/CalculatingEnergy.java
@@ -0,0 +1,21 @@
+package zh.learn.java.ch02elementary.exercises;
+
+import java.util.Scanner;
+
+public class CalculatingEnergy {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the amount of water in kilograms: ");
+        double waterKilos = input.nextDouble();
+
+        System.out.print("Enter the initial temperature: ");
+        double temperatureInit = input.nextDouble();
+
+        System.out.print("Enter the final temperature: ");
+        double temperatureFinal = input.nextDouble();
+
+        double energy = waterKilos * (temperatureFinal - temperatureInit) * 4184;
+        System.out.println("The energy needed is " + energy);
+    }
+}
