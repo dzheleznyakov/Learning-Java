53e7c98cfdaef1952e54e52ff17acb76eae74346
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-11-29 12:54:06 +0000

    Section 2

---
+++ src/main/java/zh/learn/java/ch02elementary/exercises/Acceleration.java
@@ -0,0 +1,18 @@
+package zh.learn.java.ch02elementary.exercises;
+
+import java.util.Scanner;
+
+public class Acceleration {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter v0, v1 and t: ");
+        double v0 = input.nextDouble();
+        double v1 = input.nextDouble();
+        double t = input.nextDouble();
+
+        double avgAcc = (v1 - v0) / t;
+
+        System.out.println("The average acceleration is " + avgAcc);
+    }
+}
---
+++ src/main/java/zh/learn/java/ch02elementary/exercises/CurrentTime.java
@@ -0,0 +1,26 @@
+package zh.learn.java.ch02elementary.exercises;
+
+import java.util.Scanner;
+
+public class CurrentTime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the time zone offset to GMT: ");
+        int offset = input.nextInt();
+
+        long totalMilliseconds = System.currentTimeMillis();
+        long totalSeconds = totalMilliseconds / 1000;
+        long currentSeconds = totalSeconds % 60;
+        long totalMinutes = totalSeconds / 60;
+        long currentMinute = totalMinutes % 60;
+        long totalHours = totalMinutes / 60;
+
+        long currentHour = totalHours % 24 + offset;
+        if (currentHour < 0) currentHour += 24;
+        if (currentHour >= 24) currentHour -= 24;
+
+        System.out.println("Current time is " + currentHour + ":" +
+                currentMinute + ":" + currentSeconds + " GMT");
+    }
+}
