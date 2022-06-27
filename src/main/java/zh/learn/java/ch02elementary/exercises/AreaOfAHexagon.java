7cd1fbaf6ff487a525ec72fe3b4c8bf64c993eba
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2022-06-27 20:09:04 +0100

    Section 2, exercise 16: Area of a hexagon

---
+++ src/main/java/zh/learn/java/ch02elementary/exercises/AreaOfAHexagon.java
@@ -0,0 +1,16 @@
+package zh.learn.java.ch02elementary.exercises;
+
+import java.util.Scanner;
+
+public class AreaOfAHexagon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the length of the side: ");
+        double side = input.nextDouble();
+
+        double area = 3 * Math.sqrt(3) / 2 * side * side;
+
+        System.out.println("The area of the hexagon is " + area);
+    }
+}
