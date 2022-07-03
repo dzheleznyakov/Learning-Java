85b6b3cb5f9ab5cb52961a50e7c326b8a83aebed
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2022-07-03 13:26:33 +0100

    Section 2, exercise 17: Science: wind-chill temperature

---
+++ src/main/java/zh/learn/java/ch02elementary/exercises/WindChillTemperature.java
@@ -0,0 +1,30 @@
+// section 02 exercise 17
+
+ package zh.learn.java.ch02elementary.exercises;
+
+import java.util.Scanner;
+
+public class WindChillTemperature {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the temperature in Fahrenheit between -58°F and 41°F: ");
+        double temperature = input.nextDouble();
+
+        System.out.print("Enter the wind speed (>= 2) in miles per hour: ");
+        double windSpeed = input.nextDouble();
+
+        final double FREE_COEFF = 35.74;
+        final double TEMP_COEFF = 0.6215;
+        final double SPEED_COEFF = 35.75;
+        final double SPEED_DEGREE = 0.16;
+        final double TEMP_SPEED_COEFF = 0.4275;
+
+        double windChillIndex = FREE_COEFF
+                + TEMP_COEFF * temperature
+                - SPEED_COEFF * Math.pow(windSpeed, SPEED_DEGREE)
+                + TEMP_SPEED_COEFF * temperature * Math.pow(windSpeed, SPEED_DEGREE);
+
+        System.out.println("The wind chill index is " + windChillIndex);
+    }
+}
